c94327321c0d308572bb07d251cf8a1240367549
Shernan Javier <shernanjavier@gmail.com>
2022-03-25 13:28:11 -0400

    Initial version of gateway generated by generator-jhipster@7.7.0

---
+++ jhipster_micro1/gateway/src/main/java/ca/sheridancollege/ca/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package ca.sheridancollege.ca.domain;
---
+++ jhipster_micro1/gateway/src/main/java/ca/sheridancollege/ca/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package ca.sheridancollege.ca.web.rest.vm;
