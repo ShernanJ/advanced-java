8e580f590c57bc7d5eb37b431f8ea783108fd60c
Shernan Javier <shernanjavier@gmail.com>
2022-03-25 13:20:41 -0400

    Initial version of addressAndPhone generated by generator-jhipster@7.7.0

---
+++ jhipster_micro1/addressAndPhone/src/main/java/ca/sheridancollege/javiersh/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package ca.sheridancollege.javiersh.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Address And Phone.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {}
